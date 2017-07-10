87b76639f13d49e7c1453e7952be2fde2abb6950
valentinatp <valentina.toledoporma@gmail.com>
2017-07-10 18:00:45 -0400

    Ocupo jquery agregando en el append toda la concatenación con los atributos, llamando a cada elemento de 'activities.js' para poder rellenar cada parametro

--- js/script-index.js
+++ js/script-index.js
@@ -110,8 +110,13 @@ function renderActivities(activities) {
 * archivo "templates/templates-activity.html"
 */
 function renderActivity(recipe) {
-	
-}
+	$(".list-activities").append("<a href='#' class='item-activity'>" + "<span class='attribution'>" + "<span class='avatar'>" + 
+		'<img src="' + recipe.userAvatar + '.jpg" class="image-avatar">' + '</span>' + '<span class="meta">' +
+      "<span class='author'>" + recipe.userName + "</span> made" + 
+      "<span class='recipe'>" + recipe.recipeName + "</span>" + ':' + recipe.text +
+      "<span class='location'>" + recipe.place + "</span>" + "</span>" + "</span>" +
+      '<div class="bg-image" style="background-image: url(' + recipe.image + ');"></div>' + "</a>")
+} 
 
 function ocultarFlecha(){
 	var flecha = document.getElementById('flecha');
